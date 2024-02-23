e88f439869af93cd414b7d631e3fed50e863f106
Mathéo <matheo.zeller@gmail.com>
2024-02-23 21:12:13 +0100

    :sparkles: Add home page sections + style

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      */
     .addEntry('app', './assets/app.js')
     .addEntry('nav', './assets/js/nav.js')
+    .addEntry('skills', './assets/js/skills.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
