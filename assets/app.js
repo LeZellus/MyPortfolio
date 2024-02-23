ecea6ae1abb48d3755d13df680793bbaa8811c2f
Mathéo <matheo.zeller@gmail.com>
2024-02-23 22:50:52 +0100

    :lipstick: Responsive added

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,23 @@
-/*import './bootstrap.js';*/
 import './styles/app.scss';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+docReady(function() {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
